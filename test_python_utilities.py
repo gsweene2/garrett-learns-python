c20fc9180559899573541ddceaba2d9be5c8a24f
Garrett Sweeney <garrett.d.sweeney@gmail.com>
2021-03-11 21:48:33 -0500

    better understand difference deep and shallow copy

--- test_python_utilities.py
+++ test_python_utilities.py
@@ -12,45 +12,65 @@ class TestPythonUtilities(unittest.TestCase):
         # Assert
         self.assertEqual(result, 6)
 
-    def test_shallow_copy_modify_copied(self):
+    def test_shallow_copy__modify_copied_non_object__should_not_affect_init_list(self):
         # Arrange
-        init_list = [1, 2, 3]
+        init_list = [1, 2, 3, [1, 2, 3]]
         # Act
         copy_list = Utilities.shallow_copy(init_list)
         copy_list.append(4)
         # Assert
-        self.assertEqual(init_list, [1, 2, 3])
-        self.assertEqual(copy_list, [1, 2, 3, 4])
+        self.assertEqual(copy_list, [1, 2, 3, [1, 2, 3], 4])
+        self.assertEqual(init_list, [1, 2, 3, [1, 2, 3]])
 
-    def test_shallow_copy_modify_init(self):
+    def test_shallow_copy__modify_init_non_object__should_not_affect_copy_list(self):
         # Arrange
-        init_list = [1, 2, 3]
+        init_list = [1, 2, 3, [1, 2, 3]]
         # Act
         copy_list = Utilities.shallow_copy(init_list)
         init_list.append(4)
         # Assert
-        self.assertEqual(copy_list, [1, 2, 3])
-        self.assertEqual(init_list, [1, 2, 3, 4])
+        self.assertEqual(copy_list, [1, 2, 3, [1, 2, 3]])
+        self.assertEqual(init_list, [1, 2, 3, [1, 2, 3], 4])
 
-    def test_deep_copy_modify_copied(self):
+    def test_shallow_copy__modify_object_in_init_list__should_affect_copy_list(self):
         # Arrange
-        init_list = [1, 2, 3]
+        init_list = [1, 2, 3, [1, 2, 3]]
+        # Act
+        copy_list = Utilities.shallow_copy(init_list)
+        init_list[3].append(4)
+        # Assert
+        self.assertEqual(copy_list, [1, 2, 3, [1, 2, 3, 4]])
+        self.assertEqual(init_list, [1, 2, 3, [1, 2, 3, 4]])
+
+    def test_deep_copy__modify_copied_non_object__should_not_affect_init_list(self):
+        # Arrange
+        init_list = [1, 2, 3, [1, 2, 3]]
         # Act
         copy_list = Utilities.deep_copy(init_list)
         copy_list.append(4)
         # Assert
-        self.assertEqual(copy_list, [1, 2, 3, 4])
-        self.assertEqual(init_list, [1, 2, 3])
+        self.assertEqual(copy_list, [1, 2, 3, [1, 2, 3], 4])
+        self.assertEqual(init_list, [1, 2, 3, [1, 2, 3]])
 
-    def test_deep_copy_modify_init(self):
+    def test_deep_copy__modify_init_non_object__should_not_affect_copy_list(self):
         # Arrange
-        init_list = [1, 2, 3]
+        init_list = [1, 2, 3, [1, 2, 3]]
         # Act
         copy_list = Utilities.deep_copy(init_list)
         init_list.append(4)
         # Assert
-        self.assertEqual(copy_list, [1, 2, 3])
-        self.assertEqual(init_list, [1, 2, 3, 4])
+        self.assertEqual(copy_list, [1, 2, 3, [1, 2, 3]])
+        self.assertEqual(init_list, [1, 2, 3, [1, 2, 3], 4])
+
+    def test_deep_copy__modify_object_in_init_list__should_not_affect_copy_list(self):
+        # Arrange
+        init_list = [1, 2, 3, [1, 2, 3]]
+        # Act
+        copy_list = Utilities.deep_copy(init_list)
+        init_list[3].append(4)
+        # Assert
+        self.assertEqual(copy_list, [1, 2, 3, [1, 2, 3]])
+        self.assertEqual(init_list, [1, 2, 3, [1, 2, 3, 4]])
 
     def test_get_key_from_map_else_return_default_key_exists(self):
         # Arrange
