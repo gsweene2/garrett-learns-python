d806ac5a148ebc3b525c03468c61c879d298fb9d
Garrett Sweeney <garrett.d.sweeney@gmail.com>
2021-03-31 19:39:29 -0400

    Combine methods into pytest

--- README.md
+++ README.md
@@ -1,17 +1,5 @@
 # garrett-learns-python
 
-## Simple Python Testing
-
-### unittest
-
-Sample class `python_utilities.py`
-
-Sample unittest `test_python_utilities.py`
-
-```
-python test_python_utilities.py
-```
-
 ### pytest
 
 Sample pytest `pytest_python_utilities.py`
--- pytest_python_utilities.py
+++ pytest_python_utilities.py
@@ -1,10 +1,22 @@
-from python_utilities import Utilities
+import copy
+from collections import Counter
+
+"""
+Sum
+"""
+
+def sum_list(my_list):
+    return sum(my_list)
+
+"""
+Test Sum
+"""
 
 def test_sum():
     # Arrange
     data = [1, 2, 3]
     # Act
-    result = Utilities().sum_list(data)
+    result = sum_list(data)
     # Assert
     assert result == 6
 
@@ -12,75 +24,67 @@ def test_sum():
     # Arrange
     data = [1, 2, 3]
     # Act
-    result = Utilities.sum_list(data)
+    result = sum_list(data)
     # Assert
     assert result, 6
 
-def test_shallow_copy__modify_copied_non_object__should_not_affect_init_list():
-    # Arrange
-    init_list = [1, 2, 3, [1, 2, 3]]
-    # Act
-    copy_list = Utilities.shallow_copy(init_list)
-    copy_list.append(4)
-    # Assert
-    assert copy_list == [1, 2, 3, [1, 2, 3], 4]
-    assert init_list == [1, 2, 3, [1, 2, 3]]
+"""
+Deep and Shallow Copy
+"""
 
-def test_shallow_copy__modify_init_non_object__should_not_affect_copy_list():
-    # Arrange
-    init_list = [1, 2, 3, [1, 2, 3]]
-    # Act
-    copy_list = Utilities.shallow_copy(init_list)
-    init_list.append(4)
-    # Assert
-    assert copy_list == [1, 2, 3, [1, 2, 3]]
-    assert init_list == [1, 2, 3, [1, 2, 3], 4]
+def shallow_copy(my_list):
+    return list(my_list)
+
+
+def deep_copy(my_list):
+    return copy.deepcopy(my_list)
+
+"""
+Test Deep and Shallow Copy
+"""
 
 def test_shallow_copy__modify_object_in_init_list__should_affect_copy_list():
     # Arrange
     init_list = [1, 2, 3, [1, 2, 3]]
     # Act
-    copy_list = Utilities.shallow_copy(init_list)
+    copy_list = shallow_copy(init_list)
     init_list[3].append(4)
     # Assert
     assert copy_list == [1, 2, 3, [1, 2, 3, 4]]
     assert init_list == [1, 2, 3, [1, 2, 3, 4]]
 
-def test_deep_copy__modify_copied_non_object__should_not_affect_init_list():
-    # Arrange
-    init_list = [1, 2, 3, [1, 2, 3]]
-    # Act
-    copy_list = Utilities.deep_copy(init_list)
-    copy_list.append(4)
-    # Assert
-    assert copy_list == [1, 2, 3, [1, 2, 3], 4]
-    assert init_list == [1, 2, 3, [1, 2, 3]]
-
-def test_deep_copy__modify_init_non_object__should_not_affect_copy_list():
-    # Arrange
-    init_list = [1, 2, 3, [1, 2, 3]]
-    # Act
-    copy_list = Utilities.deep_copy(init_list)
-    init_list.append(4)
-    # Assert
-    assert copy_list == [1, 2, 3, [1, 2, 3]]
-    assert init_list == [1, 2, 3, [1, 2, 3], 4]
 
 def test_deep_copy__modify_object_in_init_list__should_not_affect_copy_list():
     # Arrange
     init_list = [1, 2, 3, [1, 2, 3]]
     # Act
-    copy_list = Utilities.deep_copy(init_list)
+    copy_list = deep_copy(init_list)
     init_list[3].append(4)
     # Assert
     assert copy_list == [1, 2, 3, [1, 2, 3]]
     assert init_list == [1, 2, 3, [1, 2, 3, 4]]
 
+"""
+Get key from map
+"""
+
+
+def get_key_from_map_else_return_default(my_map, key):
+    return my_map.get(key, 'Not Found')
+
+
+def get_key_from_map_else_return_default_ternary(my_map, key):
+    return my_map[key] if key in my_map else 'Not Found'
+
+"""
+Test Get key from map
+"""
+
 def test_get_key_from_map_else_return_default_key_exists():
     # Arrange
     my_map = {'a':1,'b':2,'c':3}
     # Act
-    value = Utilities.get_key_from_map_else_return_default(my_map, 'a')
+    value = get_key_from_map_else_return_default(my_map, 'a')
     # Assert
     assert value == 1
 
@@ -88,7 +92,7 @@ def test_get_key_from_map_else_return_default_key_dne():
     # Arrange
     my_map = {'a':1,'b':2,'c':3}
     # Act
-    value = Utilities.get_key_from_map_else_return_default(my_map, 'z')
+    value = get_key_from_map_else_return_default(my_map, 'z')
     # Assert
     assert value == 'Not Found'
 
@@ -97,7 +101,7 @@ def test_get_key_from_map_else_return_default_ternary_key_exists():
     # Arrange
     my_map = {'a':1,'b':2,'c':3}
     # Act
-    value = Utilities.get_key_from_map_else_return_default_ternary(my_map, 'a')
+    value = get_key_from_map_else_return_default_ternary(my_map, 'a')
     # Assert
     assert value == 1
 
@@ -105,15 +109,30 @@ def test_get_key_from_map_else_return_default_ternary_key_dne():
     # Arrange
     my_map = {'a':1,'b':2,'c':3}
     # Act
-    value = Utilities.get_key_from_map_else_return_default_ternary(my_map, 'z')
+    value = get_key_from_map_else_return_default_ternary(my_map, 'z')
     # Assert
     assert value == 'Not Found'
+
+"""
+Sorting
+"""
+
+def sort_list_ascending(my_list):
+    return sorted(my_list)
+
+
+def sort_list_descending(my_list):
+    return sorted(my_list, reverse=True)
+
+"""
+Test Sorting
+"""
 
 def test_sort_list_ascending__should_return_sorted_list():
     # Arrange
     my_list = ['A','C','D','B','E']
     # Act
-    sorted_list = Utilities.sort_list_ascending(my_list)
+    sorted_list = sort_list_ascending(my_list)
     # Assert
     expected = ['A','B','C','D','E']
     assert sorted_list == expected
@@ -122,16 +141,30 @@ def test_sort_list_descending__should_return_sorted_list():
     # Arrange
     my_list = ['A','C','D','B','E']
     # Act
-    sorted_list = Utilities.sort_list_descending(my_list)
+    sorted_list = sort_list_descending(my_list)
     # Assert
     expected = ['E','D','C','B','A']
     assert sorted_list == expected
+
+"""
+Count Occurances in List
+"""
+
+def count_occurances_in_list(my_list, item):
+    return my_list.count(item)
+
+def count_occurances_of_each_item_in_list(my_list):
+    return Counter(my_list)
+
+"""
+Test Count Occurances in List
+"""
 
 def test_count_occurances_in_list():
     # Arrange
     my_list = [1, 3, 4, 2, 2, 2, 4, 2]
     # Act
-    occurances = Utilities.count_occurances_in_list(my_list, 2)
+    occurances = count_occurances_in_list(my_list, 2)
     # Assert
     assert 4 == occurances
 
@@ -139,7 +172,68 @@ def test_count_occurances_of_each_item_in_list():
     # Arrange
     my_list = ['a', 'c', 'd', 'b', 'b', 'b', 'c', 'b']
     # Act
-    occurances = Utilities.count_occurances_of_each_item_in_list(my_list)
+    occurances = count_occurances_of_each_item_in_list(my_list)
     # Assert
     expected = { 'a': 1, 'b': 4, 'c': 2, 'd': 1}
     assert expected == occurances
+
+"""
+Basic Comprehension
+"""
+
+def string_to_list_comprehension(my_string):
+    letter_list = [ letter for letter in my_string ]
+    return letter_list
+
+"""
+Test Basic Comprehension
+"""
+
+def test_string_to_list_comprehension():
+    # Arrange
+    my_string = 'awesome'
+    # Act
+    result_string = string_to_list_comprehension(my_string)
+    # Assert
+    expected = ['a','w','e','s','o','m','e']
+    assert expected == result_string
+
+"""
+Find Substrings in list of Strings with Comprehensions
+"""
+
+def find_strings_that_contain_substring_in_list_comprehension(list_of_strings, substring):
+    return [word for word in list_of_strings if substring in word.lower()] 
+
+"""
+Test Find Substrings in list of Strings with Comprehensions
+"""
+
+def test_find_strings_that_contain_substring_in_list_comprehension():
+    # Arrange
+    list_of_strings = ['Fred','Freedy','Reddison','Dave','Bob','Red']
+    # Act
+    result = find_strings_that_contain_substring_in_list_comprehension(list_of_strings,'red')
+    # Assert
+    expected = ['Fred','Reddison','Red']
+    assert expected == result
+
+"""
+Lambda welcome message
+"""
+
+print_welcome_lambda = lambda first, last: f"Welcome to garretts-docs, {first} {last}"
+
+"""
+Test Lambda welcome message
+"""
+
+def test_print_welcome_lambda():
+    # Arrage
+    first, last = "Garrett", "Smith"
+    # Act
+    result = print_welcome_lambda(first, last)
+    # Assert
+    expected = "Welcome to garretts-docs, Garrett Smith"
+    assert expected == result
+
