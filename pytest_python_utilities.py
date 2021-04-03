5884bf50470cbf4ee2dc901c931abfd7a4cc9c1e
Garrett Sweeney <garrett.d.sweeney@gmail.com>
2021-04-02 21:10:05 -0400

    Set up pre-commit with black formatting

--- pytest_python_utilities.py
+++ pytest_python_utilities.py
@@ -5,12 +5,15 @@ from collections import Counter
 Sum
 """
 
+
 def sum_list(my_list):
     return sum(my_list)
+
 
 """
 Test Sum
 """
+
 
 def test_sum():
     # Arrange
@@ -20,6 +23,7 @@ def test_sum():
     # Assert
     assert result == 6
 
+
 def test_sum():
     # Arrange
     data = [1, 2, 3]
@@ -28,9 +32,11 @@ def test_sum():
     # Assert
     assert result, 6
 
+
 """
 Deep and Shallow Copy
 """
+
 
 def shallow_copy(my_list):
     return list(my_list)
@@ -39,9 +45,11 @@ def shallow_copy(my_list):
 def deep_copy(my_list):
     return copy.deepcopy(my_list)
 
+
 """
 Test Deep and Shallow Copy
 """
+
 
 def test_shallow_copy__modify_object_in_init_list__should_affect_copy_list():
     # Arrange
@@ -64,58 +72,65 @@ def test_deep_copy__modify_object_in_init_list__should_not_affect_copy_list():
     assert copy_list == [1, 2, 3, [1, 2, 3]]
     assert init_list == [1, 2, 3, [1, 2, 3, 4]]
 
+
 """
 Get key from map
 """
 
 
 def get_key_from_map_else_return_default(my_map, key):
-    return my_map.get(key, 'Not Found')
+    return my_map.get(key, "Not Found")
 
 
 def get_key_from_map_else_return_default_ternary(my_map, key):
-    return my_map[key] if key in my_map else 'Not Found'
+    return my_map[key] if key in my_map else "Not Found"
+
 
 """
 Test Get key from map
 """
 
+
 def test_get_key_from_map_else_return_default_key_exists():
     # Arrange
-    my_map = {'a':1,'b':2,'c':3}
+    my_map = {"a": 1, "b": 2, "c": 3}
     # Act
-    value = get_key_from_map_else_return_default(my_map, 'a')
+    value = get_key_from_map_else_return_default(my_map, "a")
     # Assert
     assert value == 1
 
+
 def test_get_key_from_map_else_return_default_key_dne():
     # Arrange
-    my_map = {'a':1,'b':2,'c':3}
+    my_map = {"a": 1, "b": 2, "c": 3}
     # Act
-    value = get_key_from_map_else_return_default(my_map, 'z')
+    value = get_key_from_map_else_return_default(my_map, "z")
     # Assert
-    assert value == 'Not Found'
+    assert value == "Not Found"
 
 
 def test_get_key_from_map_else_return_default_ternary_key_exists():
     # Arrange
-    my_map = {'a':1,'b':2,'c':3}
+    my_map = {"a": 1, "b": 2, "c": 3}
     # Act
-    value = get_key_from_map_else_return_default_ternary(my_map, 'a')
+    value = get_key_from_map_else_return_default_ternary(my_map, "a")
     # Assert
     assert value == 1
 
+
 def test_get_key_from_map_else_return_default_ternary_key_dne():
     # Arrange
-    my_map = {'a':1,'b':2,'c':3}
+    my_map = {"a": 1, "b": 2, "c": 3}
     # Act
-    value = get_key_from_map_else_return_default_ternary(my_map, 'z')
+    value = get_key_from_map_else_return_default_ternary(my_map, "z")
     # Assert
-    assert value == 'Not Found'
+    assert value == "Not Found"
+
 
 """
 Sorting
 """
+
 
 def sort_list_ascending(my_list):
     return sorted(my_list)
@@ -124,41 +139,63 @@ def sort_list_ascending(my_list):
 def sort_list_descending(my_list):
     return sorted(my_list, reverse=True)
 
+
+def sort_dictionary_on_value(my_dict):
+    return sorted(my_dict.items(), key=lambda x: x[1])
+
+
 """
 Test Sorting
 """
 
+
 def test_sort_list_ascending__should_return_sorted_list():
     # Arrange
-    my_list = ['A','C','D','B','E']
+    my_list = ["A", "C", "D", "B", "E"]
     # Act
     sorted_list = sort_list_ascending(my_list)
     # Assert
-    expected = ['A','B','C','D','E']
+    expected = ["A", "B", "C", "D", "E"]
     assert sorted_list == expected
+
 
 def test_sort_list_descending__should_return_sorted_list():
     # Arrange
-    my_list = ['A','C','D','B','E']
+    my_list = ["A", "C", "D", "B", "E"]
     # Act
     sorted_list = sort_list_descending(my_list)
     # Assert
-    expected = ['E','D','C','B','A']
+    expected = ["E", "D", "C", "B", "A"]
     assert sorted_list == expected
+
+
+def test_sort_dictionary_on_value():
+    # Arragne
+    my_dict = {"bob": 97, "fred": 67, "katie": 78, "sophie": 99}
+    # Act
+    sorted_list = sort_dictionary_on_value(my_dict)
+    # Assert
+    expected_list = [("sophie", 99), ("bob", 97), ("katie", 78), ("fred", 67)]
+    assert expected_list == sorted_list
+
 
 """
 Count Occurances in List
 """
 
+
 def count_occurances_in_list(my_list, item):
     return my_list.count(item)
+
 
 def count_occurances_of_each_item_in_list(my_list):
     return Counter(my_list)
 
+
 """
 Test Count Occurances in List
 """
+
 
 def test_count_occurances_in_list():
     # Arrange
@@ -168,55 +205,69 @@ def test_count_occurances_in_list():
     # Assert
     assert 4 == occurances
 
+
 def test_count_occurances_of_each_item_in_list():
     # Arrange
-    my_list = ['a', 'c', 'd', 'b', 'b', 'b', 'c', 'b']
+    my_list = ["a", "c", "d", "b", "b", "b", "c", "b"]
     # Act
     occurances = count_occurances_of_each_item_in_list(my_list)
     # Assert
-    expected = { 'a': 1, 'b': 4, 'c': 2, 'd': 1}
+    expected = {"a": 1, "b": 4, "c": 2, "d": 1}
     assert expected == occurances
+
 
 """
 Basic Comprehension
 """
 
+
 def string_to_list_comprehension(my_string):
-    letter_list = [ letter for letter in my_string ]
+    letter_list = [letter for letter in my_string]
     return letter_list
+
 
 """
 Test Basic Comprehension
 """
 
+
 def test_string_to_list_comprehension():
     # Arrange
-    my_string = 'awesome'
+    my_string = "awesome"
     # Act
     result_string = string_to_list_comprehension(my_string)
     # Assert
-    expected = ['a','w','e','s','o','m','e']
+    expected = ["a", "w", "e", "s", "o", "m", "e"]
     assert expected == result_string
+
 
 """
 Find Substrings in list of Strings with Comprehensions
 """
 
-def find_strings_that_contain_substring_in_list_comprehension(list_of_strings, substring):
-    return [word for word in list_of_strings if substring in word.lower()] 
+
+def find_strings_that_contain_substring_in_list_comprehension(
+    list_of_strings, substring
+):
+    return [word for word in list_of_strings if substring in word.lower()]
+
 
 """
 Test Find Substrings in list of Strings with Comprehensions
 """
 
+
 def test_find_strings_that_contain_substring_in_list_comprehension():
     # Arrange
-    list_of_strings = ['Fred','Freedy','Reddison','Dave','Bob','Red']
+    list_of_strings = ["Fred", "Freedy", "Reddison", "Dave", "Bob", "Red"]
     # Act
-    result = find_strings_that_contain_substring_in_list_comprehension(list_of_strings,'red')
+    result = find_strings_that_contain_substring_in_list_comprehension(
+        list_of_strings, "red"
+    )
     # Assert
-    expected = ['Fred','Reddison','Red']
+    expected = ["Fred", "Reddison", "Red"]
     assert expected == result
+
 
 """
 Lambda welcome message
@@ -228,6 +279,7 @@ print_welcome_lambda = lambda first, last: f"Welcome to garretts-docs, {first} {
 Test Lambda welcome message
 """
 
+
 def test_print_welcome_lambda():
     # Arrage
     first, last = "Garrett", "Smith"
@@ -237,33 +289,38 @@ def test_print_welcome_lambda():
     expected = "Welcome to garretts-docs, Garrett Smith"
     assert expected == result
 
+
 """
 Merge Dictionaries
 """
 
+
 def merge_dictionaries(dict_1, dict_2):
     return {**dict_1, **dict_2}
+
 
 """
 Test Merge Dictionaries
 """
 
-def test_merge_dictionaries():
-    # Arrage
-    older_data = { "bob": 35, "phil": 39, "katie": 30 }
-    newer_data = { "phil": 41, "fred": 19 }
-    # Act
-    result = merge_dictionaries(older_data, newer_data)
-    # Assert
-    expected = { "bob": 35, "phil": 41, "katie": 30, "fred": 19 }
-    assert expected == result
 
 def test_merge_dictionaries():
     # Arrage
-    older_data = { "phil": 41, "fred": 19 }
-    newer_data = { "bob": 35, "phil": 39, "katie": 30 }
+    older_data = {"bob": 35, "phil": 39, "katie": 30}
+    newer_data = {"phil": 41, "fred": 19}
     # Act
     result = merge_dictionaries(older_data, newer_data)
     # Assert
-    expected = { "phil": 39, "fred": 19, "bob": 35, "katie": 30 }
+    expected = {"bob": 35, "phil": 41, "katie": 30, "fred": 19}
+    assert expected == result
+
+
+def test_merge_dictionaries():
+    # Arrage
+    older_data = {"phil": 41, "fred": 19}
+    newer_data = {"bob": 35, "phil": 39, "katie": 30}
+    # Act
+    result = merge_dictionaries(older_data, newer_data)
+    # Assert
+    expected = {"phil": 39, "fred": 19, "bob": 35, "katie": 30}
     assert expected == result
